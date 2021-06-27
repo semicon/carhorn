3e433382f14e3fec4e0ee0a0a7ad255e96bf45d9
guruchian <30399464+semicon@users.noreply.github.com>
2021-06-27 15:37:05 +0700

    Initial commit

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1624781387,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sw-sheet0.png",
+		"images/circuit-sheet0.png",
+		"images/p1-sheet0.png",
+		"images/p1-sheet1.png",
+		"images/p9-sheet0.png",
+		"images/line1.png",
+		"images/line6.png",
+		"media/mixkit-car-horn-718.m4a",
+		"media/mixkit-car-horn-718.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
